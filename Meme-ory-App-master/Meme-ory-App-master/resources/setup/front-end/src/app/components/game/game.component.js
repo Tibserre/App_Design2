3259037c2881a943cb737b62b159e58387632e9d
Tibserre <thibault.serre@epfedu.fr>
2022-04-20 14:19:20 +0200

    6.1 terminé bonsoir

--- Meme-ory-App-master/Meme-ory-App-master/resources/setup/front-end/src/app/components/game/game.component.js
+++ Meme-ory-App-master/Meme-ory-App-master/resources/setup/front-end/src/app/components/game/game.component.js
@@ -1,4 +1,4 @@
-// TODO Step 6 import { parseUrl } from '../../utils/utils.js';
+import { parseUrl } from '../../utils/utils.js';
 // TODO Step 7 import { Component } from "../../utils/component";
 // TODO Step 7 import template from "./game.component.html"
 
@@ -146,21 +146,7 @@
 
   
   // TODO Step 6: Move this method to utils.js
-  function parseUrl() {
-    let url = window.location;
-    let query = url.href.split("?")[1] || "";
-    let delimiter = "&";
-    let result = {};
-
-    let parts = query.split(delimiter);
-    
-    return parts.map(items => items.split("=")).reduce((result, kv) => {
-      result[kv[0]] = kv[1];
-      return result;
-    }, {});
-
-    
-  }
+  
 
   // put component in global scope, to be runnable right from the HTML.
   // TODO Step 7: export GameComponent
---
+++ Meme-ory-App-master/Meme-ory-App-master/resources/setup/front-end/src/app/utils/utils.js
@@ -0,0 +1,15 @@
+function parseUrl() {
+    let url = window.location;
+    let query = url.href.split("?")[1] || "";
+    let delimiter = "&";
+    let result = {};
+
+    let parts = query.split(delimiter);
+    
+    return parts.map(items => items.split("=")).reduce((result, kv) => {
+      result[kv[0]] = kv[1];
+      return result;
+    }, {});
+
+    
+  }
